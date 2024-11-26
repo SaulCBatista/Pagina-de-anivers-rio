43245f9c210c6fdf01a2d33955f9dae6046741c8
SaulCBatista <saulcbatista@gmail.com>
2024-11-26 18:41:34 -0300

    add animation

---
+++ src/scripts/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    ScrollReveal().reveal('#titulo', {
+        origin: 'top',
+        duration: 2000,
+        distance: '20%'
+    });
+
+    ScrollReveal().reveal('#texto_brenda', {
+        origin: 'left',
+        duration: 2000,
+        distance: '20%'
+    });
+
+    ScrollReveal().reveal('#imagens_brenda', {
+        origin: 'right',
+        duration: 2000,
+        distance: '20%'
+    });
+
+    ScrollReveal().reveal('#texto_casal', {
+        origin: 'right',
+        duration: 2000,
+        distance: '20%'
+    });
+
+    ScrollReveal().reveal('.imagens-casal', {
+        origin: 'left',
+        duration: 2000,
+        distance: '20%'
+    });
+});
